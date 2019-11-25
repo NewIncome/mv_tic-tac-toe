9a2354ba2636114c2ec8a075c768072bb6ada5b1
NewIncome <alfredo_cardenas@live.com>
2019-11-25 15:19:06 -0600

    Fix one last linter error

--- lib/board.rb
+++ lib/board.rb
@@ -2,9 +2,11 @@
 
 class Board
   attr_accessor :board_cells
+
   def initialize
     @board_cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
+
   def display
     puts ''
     puts "\t \t #{@board_cells[0]} | #{@board_cells[1]} | #{@board_cells[2]}"
