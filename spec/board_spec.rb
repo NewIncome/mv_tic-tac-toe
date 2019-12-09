8e0238d513fa63cf4fcf6b1e35a883dca462f62d
NewIncome <alfredo_cardenas@live.com>
2019-12-09 14:16:09 -0600

    Fixed linter errors.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,9 @@
-require_relative("../lib/board")
+# frozen_string_literal: true
+
+require_relative('../lib/board')
 
 RSpec.describe(Board) do
-  let(:board) {Board.new}
+  let(:board) { Board.new }
 
   it('has cells') do
     expect(board.board_cells).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9]))
@@ -11,7 +13,7 @@ RSpec.describe(Board) do
     board.board_cells[5] = 'X'
     expect(board.board_cells).to(eq([1, 2, 3, 4, 5, 'X', 7, 8, 9]))
   end
-  
+
   it('can display a game board') do
     expect(board.display).to(eq("
     \t \t #{board.board_cells[0]} | #{board.board_cells[1]} | #{board.board_cells[2]}
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "../lib/player"
+require_relative '../lib/player'
 
 RSpec.describe(Player) do
-  let(:player) {Player.new('Player 1', 'X')}
+  let(:player) { Player.new('Player 1', 'X') }
 
   it('has a name') do
     expect(player.name).to(eq('Player 1'))
@@ -30,5 +30,4 @@ RSpec.describe(Player) do
     player.make_choice(6)
     expect(player.choices).to(eq([6]))
   end
-
 end
